f8f2d4db3836b16661a2df21ddd1830b2bf2a86b
Tracey <tracey@steinbacholiver.com>
2016-03-27 16:32:01 +1000

    Adding simplebuild-jshint

--- Jakefile.js
+++ Jakefile.js
@@ -57,16 +57,16 @@
 
     desc("Run unit tests");
     task("unit-tests", function(){
-        console.log("Placeholder for unit tests.")
+        console.log("Placeholder for unit tests.");
     });
 
     desc("Run integration tests");
     task("integration-tests", function() {
-        console.log("Placeholder for integration tests.")
+        console.log("Placeholder for integration tests.");
     });
 
     desc("Run acceptance tests");
     task("acceptance-tests", function(){
-        console.log("Placeholder for acceptance tests.")
+        console.log("Placeholder for acceptance tests.");
     });
 }());
