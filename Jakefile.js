f7c3c55a2927da37ac0aeaf23ea5717320d9492a
Tracey <tracey@steinbacholiver.com>
2016-03-27 14:37:36 +1000

    Installed Jake Locally

--- Jakefile.js
+++ Jakefile.js
@@ -4,22 +4,22 @@
     desc("Default build.");
     task("default", function(){
        console.log("Hello, I'm the default task");
+        console.log("\n\nBUILD OK");
     });
 
     desc("Run unit tests");
+
     task("unit-tests", function(){
         console.log("Placeholder for unit tests.")
     });
-
     desc("Run integration tests");
+
     task("integration-tests", function() {
         console.log("Placeholder for integration tests.")
     });
-
     desc("Run acceptance tests");
+
     task("acceptance-tests", function(){
         console.log("Placeholder for acceptance tests.")
     });
-
-    console.log("\n\nBUILD OK");
 }());
