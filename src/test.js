f1e4e2ddb1b5958d03f02b92ec4ed631170dd3a7
Tracey <tracey@steinbacholiver.com>
2016-03-29 08:37:43 +1000

    adding mocha to test

--- src/test.js
+++ src/test.js
@@ -1,6 +1,4 @@
 //Created by tracey on 12/12/2015.
-
-
 (function () {
     "use strict";
 
@@ -9,12 +7,18 @@
         expect = chai.expect,
         should = chai.should();
 
-    assert.equal(add(3, 4), 7);
-    expect(add(3, 4)).to.equal(7);
-    add(3, 4).should.equal(7);
+    describe("Addition", function(){
+
+        it("add 2 numbers", function() {
+            assert.equal(add(3, 4), 7);
+        });
+
+        it("IEEE 754 floating point", function() {
+            assert.equal(add(0.1, 0.2), 0.30000000000000004);
+        });
+    });
 
     function add(a, b) {
-        return a +   b;
+        return a + b;
     }
-
 }());
