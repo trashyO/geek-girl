6f005269f50cb063c14c9ca22e9dddd7d729d7c3
Tracey <tracey@steinbacholiver.com>
2016-03-29 11:46:36 +1000

    Added karma and run tests to build. Refactor tests to use own assertEqual method. Until modules topic introduced

--- Jakefile.js
+++ Jakefile.js
@@ -13,19 +13,20 @@
 
     //******* Global Tasks *******
     desc("Start Karma");
-    task("karma", function(){
-        karma.start({configFile: "karma.conf.js"}, complete, fail), {async: true}
-    });
+    task("karma", function() {
+        console.log("Starting karma: ");
+        karma.start({configFile: "karma.conf.js"}, complete, fail);
+    }, {async: true});
 
     desc("Default build.");
-    task("default", ["check-version", "lint"], function(){
-       console.log("Hello, I'm the default task");
+    task("default", ["check-version", "lint", "unit-tests"], function(){
+       console.log("Running default: ");
         console.log("\n\nBUILD OK");
     });
 
     desc("Run http server");
     task("run", function(){
-        console.log("Running http-server");
+        console.log("Running http-server: ");
         jake.exec("node node_modules/http-server/bin/http-server src", {interactive: true}, complete);
     });
 
@@ -74,8 +75,9 @@
 
     desc("Run unit tests");
     task("unit-tests", function(){
-        console.log("Placeholder for unit tests.");
-    });
+        console.log("Running unit tests: ");
+        karma.run({configFile: "karma.conf.js"}, complete, fail);
+    }, {async: true});
 
     desc("Run integration tests");
     task("integration-tests", function() {
