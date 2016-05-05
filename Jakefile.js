22eab8ab4d875583be6466d79c2d2c4f2632bdd4
Tracey <tracey@steinbacholiver.com>
2016-05-05 20:31:18 +1000

    Update OS version to 10.10.5 for karma cross browser testing

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,7 @@
 // Created by Tracey Oliver on 12/12/2015
-/* globals jake:false, desc:false, task:false, fail: alse, complete:false  */
+/* globals jake:false, desc:false, task:false, fail:false, complete:false  */
 
-(function() {
+(function () {
     "use strict";
 
     let packageJson = require("./package.json");
@@ -14,7 +14,7 @@
 
     //******* Global Tasks *******
     desc("Start Karma");
-    task("karma", function() {
+    task("karma", function () {
         console.log("Starting karma: ");
         karma.start({
             configFile: KARMA_CONF
@@ -22,61 +22,62 @@
     }, {async: true});
 
     desc("Default build.");
-    task("default", ["check-version", "lint", "test"], function(){
-       console.log("Running default: ");
+    task("default", ["check-version", "lint", "test"], function () {
+        console.log("Running default: ");
         console.log("\n\nBUILD OK");
     });
 
     desc("Run http server");
-    task("run", function(){
+    task("run", function () {
         console.log("Running http-server: ");
         jake.exec("node node_modules/http-server/bin/http-server src", {interactive: true}, complete);
     });
 
     //******* Local Tasks *******
     desc("Check external dependency versions");
-    task("check-version", function() {
+    task("check-version", function () {
         console.log("Checking node version: .");
         let actualNodeVersion = process.version;
 
-        if(semver.neq(actualNodeVersion, EXPECTED_NODE_VERSION)) {
+        if (semver.neq(actualNodeVersion, EXPECTED_NODE_VERSION)) {
             fail(`Incorrect version of node. Expected: ${EXPECTED_NODE_VERSION} Actual: ${actualNodeVersion}`);
         }
     });
 
     desc("Linting javascript");
-    task("lint", function() {
+    task("lint", function () {
         process.stdout.write("Linting JavaScript: ");
 
         jshint.checkFiles({
             files: ["Jakefile.js", "src/**/*.js"],
             options: lintOptions(),
-            globals: {desc, task
+            globals: {
+                desc, task
             }
         }, complete, fail);
     }, {async: true});
 
 
     desc("Run tests");
-    task("test", function() {
-        console.log("Testing JavaScript:");
+    task("test", function () {
+        console.log("Testing JavaScript: ");
         karma.run({
             configFile: KARMA_CONF,
             expectedBrowsers: [
-                "Chrome 48.0.2564 (Mac OS X 10.10.4)",
-                "Safari 8.0.7 (Mac OS X 10.10.4)"
+                "Chrome 50.0.2661 (Mac OS X 10.10.5)",
+                "Safari 8.0.8 (Mac OS X 10.10.5)"
             ],
             strict: !process.env.skipbrowsers
         }, complete, fail);
-    }, { async: true });
+    }, {async: true});
 
     desc("Run integration tests");
-    task("integration-tests", function() {
+    task("integration-tests", function () {
         console.log("Placeholder for integration tests.");
     });
 
     desc("Run acceptance tests");
-    task("acceptance-tests", function(){
+    task("acceptance-tests", function () {
         console.log("Placeholder for acceptance tests.");
     });
 
